f0c40c8f99f7edb58c7a707e8c77d80357caf827
李骥平 <lijiping@demohour.com>
2011-11-10 18:19:37 +0800

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def share
+    if params[:user_id] && params[:support]
+    else
+    end
+    respond_to do |format|
+      format.js
+    end
+  end
 end
